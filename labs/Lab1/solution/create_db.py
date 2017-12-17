bb30f853b70e822e58f95e4940f1f6c694e124ba
Braun Brelin <bbrelin@gmail.com>
2017-12-17 23:25:38 +0000

    Added Slides and PDF, and Lab4 and Lab solutions for Labs 1 and 2

---
+++ labs/Lab1/solution/create_db.py
@@ -0,0 +1,81 @@
+#!/usr/bin/env python3
+
+import psycopg2
+import csv
+
+''' Solution to Lab 1 Drop and Create automation 
+'''
+
+def create_tables():
+    create_tables  = [ ''' CREATE TABLE IF NOT EXISTS source (
+                                                   source_id SERIAL PRIMARY KEY NOT NULL,
+                                                   source VARCHAR (50)
+                                                  )
+                       ''',
+                       ''' CREATE TABLE IF NOT EXISTS subject (
+                                                  subject_id SERIAL PRIMARY KEY NOT NULL,
+                                                  subject VARCHAR (50)
+                                           )
+                       ''',
+
+                       ''' CREATE TABLE IF NOT EXISTS publisher (
+                                                  publisher_id SERIAL PRIMARY KEY NOT NULL,
+                                                  publisher VARCHAR (50)
+                                               )
+                       ''',
+                       ''' CREATE TABLE IF NOT EXISTS ebook (
+                                                  isbn VARCHAR (17) PRIMARY KEY NOT NULL,
+                                                  source_id SERIAL REFERENCES source(source_id),
+                                                  subject_id  SERIAL REFERENCES subject(subject_id),
+                                                  publisher_id SERIAL REFERENCES publisher(publisher_id),
+                                                  book_title VARCHAR (50),
+                                                  release_year VARCHAR(4),
+                                                  status VARCHAR(8),
+                                                  availability DATE,
+                                                  remarks VARCHAR(100)
+                                             )
+                       '''
+                  
+                 ]
+    try:
+        for command in create_tables:
+           cur.execute (command)
+    except (Exception, psycopg2.DatabaseError) as error:
+        print ('Error in create: ',error)
+
+    connect.commit()
+    return
+
+def drop_tables():
+    drop_commands = [ 'DROP TABLE IF EXISTS source CASCADE',
+                      'DROP TABLE IF EXISTS subject CASCADE  ',
+                      'DROP TABLE IF EXISTS publisher CASCADE ',
+                      'DROP TABLE IF EXISTS ebook CASCADE'
+                    ]
+
+    try:
+        for command in drop_commands:
+            cur.execute (command)
+    except (Exception, psycopg2.DatabaseError) as error:
+            print ('Error in drop: ',error)
+    connect.commit()
+    return
+
+
+def main()
+    try:
+        connect = psycopg2.connect('dbname = ebooks user=student1 password=student1:123')
+    except: 
+        print ('Connect Error')
+    cur = connect.cursor()
+
+    drop_tables()
+    create_tables()
+    connect.close()
+
+if __name__ == '__main__':
+    main()
+
+
+
+
